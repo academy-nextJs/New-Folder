d110237bb0730f34c6742fb70a125ec033b787a0
aref <aref@gmail.com>
2025-04-24 13:27:37 +0330

    add fetch interceptor and register page logic

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import {api} from "../../core/interceptore/fetchApi"
+
+
+export default async function Home() {
+  const res = await api.get('/houses')
+
+  console.log(res)
   return (
     <div></div>
   );
--- src/components/auth/container.tsx
+++ src/components/auth/container.tsx
@@ -2,7 +2,7 @@ import authBanner from "../../assets/auth banner.svg";
 import Image from "next/image";
 import { Icon } from "lucide-react";
 
-const Container = ({children}) => {
+const Container = ({children}:{children: React.ReactNode;}) => {
   return (
     <div className="flex  text-white mt-10">
       <div className="w-1/2">
