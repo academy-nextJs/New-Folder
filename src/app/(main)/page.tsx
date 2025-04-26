92cd80dac58bbb6b9713a3429354a82c2cbab929
KianJanloo <ddrdslayer@gmail.com>
2025-04-26 23:55:24 +0330

    Fixed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import AboutUs from "@/components/Landing/aboutUs";
 import { fetchApi } from "../../core/interceptore/fetchApi";
+import SpecialOffer from "@/components/Landing/SpecialOffer";
 
 export default async function Home() {
   const res = await fetchApi.get("/houses");
@@ -7,8 +8,15 @@ export default async function Home() {
   console.log(res);
   return (
 
-    <div className=" text-primary font-bold w-full h-100">
-      <AboutUs />
+    <div className="w-full flex flex-col gap-16 my-[100px]">
+      <div className="relative">
+        <div className="w-[218] h-[218] bg-[#7569FF52] blur-[256px] absolute right-[-150px]"> </div>
+        <SpecialOffer />
+      </div>
+      <div className="relative">
+        <div className="w-[218] h-[218] bg-[#8CFF4552] blur-[256px] absolute left-[-150px]"> </div>    
+        <AboutUs />
+      </div>
     </div>
   );
 }
---
+++ src/components/Landing/SpecialOffer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SpecialOffer = () => {
+  return (
+    <div className="w-full h-[744] bg-[#2D2D2D] rounded-[40px] relative after:content-['']
+        after:w-[220px] after:h-[100px] after:group-hover:bg-primary after:absolute after:top-[-50]
+         after:rounded-tr-[40px] after:rounded-tl-[70px] after:right-0 after:bg-[#2D2D2D]"
+    >
+      <div></div>
+    </div>
+  )
+}
+
+export default SpecialOffer
