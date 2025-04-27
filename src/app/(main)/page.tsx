6535b71bbbf3afff506c3d003510a27315beb677
mehdi Qasemi <mehdi.q6991@gmail.com>
2025-04-27 20:13:51 +0330

    to pull from main branch

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,27 @@
 import AboutUs from "@/components/Landing/aboutUs";
 import { fetchApi } from "../../core/interceptore/fetchApi";
 import SpecialOffer from "@/components/Landing/SpecialOffer";
+import HeroSection from "@/components/Landing/hero-section/HeroSection";
 export default async function Home() {
   const res = await fetchApi.get("/houses");
 
   console.log(res);
   return (
+    <>
+      <div className="w-full flex flex-col gap-16 my-[-81px]">
+        <HeroSection />
+        <div className="relative">
+          <div className="w-[218] h-[218] bg-[#7569FF52] blur-[256px] z-10 absolute right-[-150px]"></div>
 
-    <div className="w-full flex flex-col gap-16 my-[100px]">
-      <div className="relative">
-        <div className="w-[218] h-[218] bg-[#7569FF52] blur-[256px] z-10 absolute right-[-150px]"> </div>
-        <SpecialOffer />
+          <SpecialOffer />
+        </div>
+        <div className="relative">
+          <div className="w-[218] h-[218] bg-[#8CFF4552] blur-[256px] absolute left-[-150px]">
+            {" "}
+          </div>
+          <AboutUs />
+        </div>
       </div>
-      <div className="relative">
-        <div className="w-[218] h-[218] bg-[#8CFF4552] blur-[256px] absolute left-[-150px]"> </div>    
-        <AboutUs />
-      </div>
-    </div>
+    </>
   );
 }
