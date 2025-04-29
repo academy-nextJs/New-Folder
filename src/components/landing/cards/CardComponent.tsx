77e7103cbb73574e0679bb5b0050bf0c766c75fb
KianJanloo <ddrdslayer@gmail.com>
2025-04-29 14:43:58 +0330

    Fixed

--- src/components/landing/cards/CardComponent.tsx
+++ src/components/landing/cards/CardComponent.tsx
@@ -9,7 +9,7 @@ const CardComponent: FC<ICard> = ({ view, image, rate, title, address, rooms, pa
     const discount_percentage = discountedPrice ? Math.ceil(((Number(price) - discountedPrice) / Number(price)) * 100) : 0
 
     return (
-        <motion.div className='flex flex-col gap-4 text-white group w-fit h-full my-10 mx-auto'>
+        <motion.div whileHover={{ scale: 1.02 }} transition={{ duration: 0.2 }} className='flex flex-col gap-4 text-white group w-fit h-full my-10 mx-auto'>
             <div className='relative '>
                 <div className="bg-secondary-light2 w-full p-4 group-hover:bg-primary relative h-[190] rounded-[24px] flex justify-center items-center after:content-['']
                         after:w-[90px] after:h-[50px] after:group-hover:bg-primary after:absolute after:top-[-34] after:rounded-tr-2xl after:rounded-tl-[40px] after:right-0 after:bg-secondary-light2">
