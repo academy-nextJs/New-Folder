6ebf15cb4b2f8a8fcbbc698d545c150381a07027
KianJanloo <ddrdslayer@gmail.com>
2025-04-30 18:44:45 +0330

    Fixed

--- src/app/(main)/(auth)/login/page.tsx
+++ src/app/(main)/(auth)/login/page.tsx
@@ -24,21 +24,21 @@ const LoginPage = () => {
         </div>
         <div className="flex gap-2 flex-row-reverse items-center text-sm w-full justify-center text-subText">
           <svg width="251" height="2" viewBox="0 0 251 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M249.875 1L1.75001 0.999978" stroke="url(#paint0_linear_1_8425)" stroke-width="2" strokeLinecap="round" />
+            <path d="M249.875 1L1.75001 0.999978" stroke="url(#paint0_linear_1_8425)" strokeWidth="2" strokeLinecap="round" />
             <defs>
               <linearGradient id="paint0_linear_1_8425" x1="249.875" y1="0.5" x2="1.75" y2="0.499978" gradientUnits="userSpaceOnUse">
-                <stop offset="0.06" stop-color="#AAAAAA" stopOpacity="0.94" />
-                <stop offset="1" stop-color="#AAAAAA" stopOpacity="0" />
+                <stop offset="0.06" stopColor="#AAAAAA" stopOpacity="0.94" />
+                <stop offset="1" stopColor="#AAAAAA" stopOpacity="0" />
               </linearGradient>
             </defs>
           </svg>
           <span className='whitespace-nowrap'> یا میتوانید </span>
           <svg width="251" height="2" viewBox="0 0 251 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.875 1H250" stroke="url(#paint0_linear_1_8424)" stroke-width="2" strokeLinecap="round" />
+            <path d="M1.875 1H250" stroke="url(#paint0_linear_1_8424)" strokeWidth="2" strokeLinecap="round" />
             <defs>
               <linearGradient id="paint0_linear_1_8424" x1="1.875" y1="1.5" x2="250" y2="1.5" gradientUnits="userSpaceOnUse">
-                <stop offset="0.06" stop-color="#AAAAAA" stopOpacity="0.94" />
-                <stop offset="1" stop-color="#AAAAAA" stopOpacity="0" />
+                <stop offset="0.06" stopColor="#AAAAAA" stopOpacity="0.94" />
+                <stop offset="1" stopColor="#AAAAAA" stopOpacity="0" />
               </linearGradient>
             </defs>
           </svg>
