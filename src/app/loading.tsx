a0d118a1e222ae39f8fce4830a38f0dad7018913
KianJanloo <ddrdslayer@gmail.com>
2025-05-03 16:41:52 +0330

    Debug loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,20 @@
-'use client'
-import { Loader } from '@/components/common/Loader'
-import React from 'react'
+'use client';
+import React, { useEffect } from 'react';
 
 const Loading = () => {
-  return (
-    <div className='w-screen h-screen inset-0 flex justify-center items-center mx-auto'>
-      <Loader />
-    </div>
-  )
-}
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
 
-export default Loading
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
+  return (
+    <div className="w-screen h-screen fixed z-[10000] bg-background flex justify-center items-center mx-auto">
+      <div className="w-16 h-16 border-4 dark:border-green-500 border-black border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
