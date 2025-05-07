f165562b3543853c10721895c06372e4c01acd7f
KianJanloo <ddrdslayer@gmail.com>
2025-05-07 14:54:47 +0330

    Location

--- src/components/rent/RentMap.tsx
+++ src/components/rent/RentMap.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import ReserveMap from "../reserve/map/ReserveMap";
 import { motion } from 'framer-motion'
 
-const RentMap = ({ caption }: { caption: string }) => {
+const RentMap = ({ caption, location }: { caption: string, location: { lng: number, lat: number } }) => {
   return (
     <motion.div initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }} className="w-full flex flex-col md:flex-row justify-center gap-6">
       <div className="w-1/2 max-md:hidden h-[324px]">
-        <ReserveMap />
+        <ReserveMap location={location} />
       </div>
       <div className="w-full md:hidden cursor-pointer block h-[324px]">
-        <ReserveMap />
+        <ReserveMap location={location} />
       </div>
 
       <div className="w-1/2 max-md:w-full">
--- src/components/rent/Tab.tsx
+++ src/components/rent/Tab.tsx
@@ -64,7 +64,7 @@ export default function PropertyTabs({ house }: { house: IHouse }) {
             ))}
           </motion.div>
         )}
-        {activeTab === "location" && <RentMap caption={house.caption || 'هیج اطلاعاتی موجود نیست'} />}
+        {activeTab === "location" && <RentMap location={house.location} caption={house.caption || 'هیج اطلاعاتی موجود نیست'} />}
         {activeTab === "reviews" && (
           <motion.div initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}>
