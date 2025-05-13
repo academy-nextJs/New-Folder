bf31821a54e50174b7dd146cf9c51d5a259c0793
KianJanloo <ddrdslayer@gmail.com>
2025-05-13 23:34:38 +0330

    Debug

--- src/components/Landing/hero-section/section/InfoIndicator.tsx
+++ src/components/Landing/hero-section/section/InfoIndicator.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import Frame from "@/assets/images/heroSection/Frame.svg";
+import { TypingAnimation } from "@/components/magicui/typing-animation";
 
 const InfoIndicator = () => {
+
   return (
-    <div className="hidden sm:flex flex-cols text-right justify-center items-center align-center w-[25%] ">
-      <div className=" text-foreground">
-        <p className="md:text-xl sm:text-xs mb-1"> بیش از 7000+ </p>
-        <p className="text-sm sm:text-xs">
-          {" "}
-          رضایت مشتریانی که به دلتا اعتماد کرده اند{" "}
-        </p>
+    <div className=" text-right flex items-center  ">
+      <div className="mb-4 text-white">
+        <TypingAnimation className="md:text-xl sm:text-xs mb-1">  بیش از 7000+  </TypingAnimation>
+        <TypingAnimation delay={1500} className="text-sm sm:text-xs"> رضایت مشتریانی که به دلتا اعتماد کرده اند </TypingAnimation>
       </div>
-      <Image
-        src={Frame}
-        className="relative lg:block md:hidden hidden  top-8"
-        alt="Frame"
-        width={45}
-        height={46}
-      />
+      <Image src={Frame} className="relative top-8" alt="Frame" width={45} height={46} />
     </div>
   );
 };
