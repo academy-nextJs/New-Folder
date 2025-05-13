3a4c7f0c618931bf47739fc11588e308aaa57fe3
KianJanloo <ddrdslayer@gmail.com>
2025-05-13 23:34:09 +0330

    Debug

--- src/components/dashboard/layout/sidebar/SidebarDashboard.tsx
+++ src/components/dashboard/layout/sidebar/SidebarDashboard.tsx
@@ -1,33 +1,19 @@
-"use client";
+'use client';
 
-import {
-  Heart,
-  Home,
-  LogOut,
-  Text,
-  User,
-  MoreHorizontal,
-  X,
-  LogIn,
-} from "lucide-react";
-import React, { useEffect, useRef, useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import DeltaIcon from "@/app/icon.png";
-import { usePathname } from "next/navigation";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { Heart, Home, LogOut, Text, User, MoreHorizontal, X, LogIn } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import DeltaIcon from '@/app/icon.png';
+import { usePathname } from 'next/navigation';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 const routes = [
-  { label: "داشبورد", href: "/dashboard", icon: Home },
-  { label: "اطلاعات کاربری", href: "/dashboard/profile", icon: User },
-  { label: "ذخیره‌ها", href: "/dashboard/favorites", icon: Heart },
-  { label: "پرداخت های من", href: "/dashboard/my-payments", icon: Heart },
-  { label: "دیدگاه‌های من", href: "/dashboard/my-comments", icon: Text },
+  { label: 'داشبورد', href: '/dashboard', icon: Home },
+  { label: 'اطلاعات کاربری', href: '/dashboard/profile', icon: User },
+  { label: 'ذخیره‌ها', href: '/dashboard/favorites', icon: Heart },
+  { label: 'پرداخت های من', href: '/dashboard/my-payments', icon: Heart },
+  { label: 'دیدگاه‌های من', href: '/dashboard/my-comments', icon: Text },
 ];
 
 const SidebarDashboard = ({
@@ -51,43 +37,29 @@ const SidebarDashboard = ({
       }
     };
     if (showMore) {
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside);
     }
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [showMore]);
 
   return (
     <>
-      <div
-        className={`bg-subBg md:flex hidden transition-all duration-300 ease-in-out px-4 border py-8 gap-8 rounded-xl w-fit flex-col shadow-md ${
-          view === 1
-            ? "opacity-100 scale-100 pointer-events-auto"
-            : "opacity-0 scale-95 pointer-events-none absolute"
-        }`}
-      >
+      <div className={`bg-subBg md:flex hidden transition-all duration-300 ease-in-out px-4 border py-8 gap-8 rounded-xl w-fit flex-col shadow-md ${view === 1 ? 'opacity-100 scale-100 pointer-events-auto' : 'opacity-0 scale-95 pointer-events-none absolute'}`}>
         <div className="flex justify-between items-center mb-6 min-w-[200px]">
-          <Link href={"/"} className="text-2xl font-bold">
-            دلتا
-          </Link>
-          <LogOut
-            onClick={() => setView(2)}
-            className="cursor-pointer hover:text-accent transition-colors"
-          />
+          <Link href={'/'} className="text-2xl font-bold">دلتا</Link>
+          <LogOut onClick={() => setView(2)} className="cursor-pointer hover:text-accent transition-colors" />
         </div>
         <div className="flex flex-col gap-2">
           {routes.map(({ label, href, icon: Icon }) => {
-            const isActive = pathname.startsWith(href);
+            const isActive = pathname == href;
             return (
               <Link
                 key={href}
                 href={href}
-                className={`whitespace-nowrap flex gap-3 items-center px-3 py-2 rounded-lg font-medium transition-colors ${
-                  isActive
-                    ? "dark:bg-accent bg-subBg2 dark:text-accent-foreground"
-                    : "hover:bg-subBg2"
-                }`}
+                className={`whitespace-nowrap flex gap-3 items-center px-3 py-2 rounded-lg font-medium transition-colors ${isActive ? 'dark:bg-accent dark:text-accent-foreground bg-subBg2' : 'hover:bg-subBg2 bg-none'
+                  }`}
               >
                 <Icon className="min-w-5 min-h-5 h-5 w-5" />
                 <span>{label}</span>
@@ -97,63 +69,48 @@ const SidebarDashboard = ({
         </div>
       </div>
 
-      <div
-        className={`bg-subBg md:flex hidden transition-all duration-300 ease-in-out px-4 border py-8 gap-8 rounded-xl w-fit flex-col shadow-md ${
-          view === 2
-            ? "opacity-100 scale-100 pointer-events-auto"
-            : "opacity-0 scale-95 pointer-events-none absolute"
-        }`}
-      >
+      <div className={`bg-subBg md:flex hidden transition-all duration-300 ease-in-out px-4 border py-8 gap-8 rounded-xl w-fit flex-col shadow-md ${view === 2 ? 'opacity-100 scale-100 pointer-events-auto' : 'opacity-0 scale-95 pointer-events-none absolute'}`}>
         <div className="flex justify-center items-center mb-6">
-          <LogIn
-            onClick={() => setView(1)}
-            className="cursor-pointer rotate-180 hover:text-accent transition-colors"
-          />
+          <LogIn onClick={() => setView(1)} className="cursor-pointer rotate-180 hover:text-accent transition-colors" />
         </div>
         <div className="flex flex-col gap-2">
           {routes.map(({ label, href, icon: Icon }) => {
-            const isActive = pathname.startsWith(href);
+            const isActive = pathname === href
             return (
               <Link
                 key={href}
                 href={href}
-                className={`whitespace-nowrap flex gap-3 items-center px-3 py-2 rounded-lg font-medium transition-colors ${
-                  isActive
-                    ? "dark:bg-accent bg-subBg2 dark:text-accent-foreground"
-                    : "hover:bg-subBg2"
-                }`}
+                className={`whitespace-nowrap flex gap-3 items-center px-3 py-2 rounded-lg font-medium transition-colors hover:bg-subBg2 ${isActive ? 'dark:bg-accent dark:text-accent-foreground bg-subBg2' : 'hover:bg-subBg2 bg-none'
+                  }`}
               >
                 <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger>
                       <Icon className="w-5 h-5 min-w-5 min-h-5" />
                     </TooltipTrigger>
-                    <TooltipContent className="dark:bg-accent bg-subBg2 dark:accent-foreground text-foreground">
+                    <TooltipContent className='dark:bg-accent bg-subBg2 dark:accent-foreground absolute right-6 whitespace-nowrap text-foreground'>
                       <p> {label} </p>
                     </TooltipContent>
                   </Tooltip>
                 </TooltipProvider>
+
               </Link>
             );
           })}
         </div>
       </div>
 
-      <div
-        className={`w-dvw fixed bottom-0 right-0 z-50 justify-around items-center bg-subBg border-t py-3 md:hidden flex`}
-      >
+      <div className={`w-dvw fixed bottom-0 right-0 z-50 justify-around items-center bg-subBg border-t py-3 md:hidden flex`}>
         {mainRoutes.map(({ href, icon: Icon, label }) => {
-          const isActive = pathname.startsWith(href);
+          const isActive = pathname === href
           return (
             <Link key={href} href={href} className="flex flex-col items-center">
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger>
-                    <Icon
-                      className={`w-6 h-6 ${isActive ? "text-primary" : ""}`}
-                    />
+                    <Icon className={`w-6 h-6 ${isActive ? 'text-primary' : ''}`} />
                   </TooltipTrigger>
-                  <TooltipContent className="dark:bg-accent bg-subBg2 dark:accent-foreground text-foreground">
+                  <TooltipContent className='dark:bg-accent bg-subBg2 dark:accent-foreground text-foreground'>
                     <p> {label} </p>
                   </TooltipContent>
                 </Tooltip>
@@ -180,16 +137,12 @@ const SidebarDashboard = ({
                 </button>
                 {extraRoutes.map(({ href, label, icon: Icon }) => {
                   const isActive = pathname.startsWith(href);
-
                   return (
                     <Link
                       key={href}
                       href={href}
-                      className={`flex items-center gap-2 px-2 py-1 flex-nowrap whitespace-nowrap rounded-md text-sm ${
-                        isActive
-                          ? "bg-accent text-accent-foreground"
-                          : "hover:bg-subBg2"
-                      }`}
+                      className={`flex items-center gap-2 px-2 py-1 flex-nowrap whitespace-nowrap rounded-md text-sm ${isActive ? 'bg-accent text-accent-foreground' : 'hover:bg-subBg2'
+                        }`}
                       onClick={() => setShowMore(false)}
                     >
                       <Icon className="min-w-4 min-h-4 h-4 w-4" />
