0a77bcefdf1c3b3af51496571002644701623934
mehdi Qasemi <mehdi.q6991@gmail.com>
2025-05-13 17:31:31 +0330

    Bug:NF-64:some landing hero section bugs fixed

--- src/components/Landing/hero-section/HeroSection.tsx
+++ src/components/Landing/hero-section/HeroSection.tsx
@@ -21,7 +21,7 @@ const HeroSection = () => {
   }, [theme]);
 
   return (
-    <div className="relative max-md:hidden w-full h-[500px] sm:h-[565px] md:h-[775px] lg:h-[901px] xl:h-[901px] 2xl:h-[901px] ">
+    <div className="relative w-full h-[400px] sm:h-[465px] md:h-[675px] lg:h-[701px] xl:h-[751px] 2xl:h-[901px] ">
       <div className="absolute inset-0 z-0">
         <Image
           src={heroSection}
@@ -35,21 +35,19 @@ const HeroSection = () => {
       <motion.div
         initial={{ right: 1000 }}
         whileInView={{ right: 0 }}
-        className="absolute bottom-0 left-1/2 lg:left-3/8 md:left-1/2 sm:left-3/8 transform -translate-x-1/4 md: z-40"
+        className="absolute bottom-0 left-1/2 lg:left-3/8 md:left-1/2 sm:left-3/8 transform translate-x-1/4 sm:-translate-x-1/4  z-40"
       >
         <Image src={building} alt="building" className="object-contain" />
       </motion.div>
 
-      <div className="relative -top-10 z-20 h-full w-full px-8 py-6 gap-8 flex flex-col justify-center">
+      <div className="relative -top-10 z-20 h-full w-full px-8 py-2 gap-2 flex flex-col justify-center">
         <div className="flex flex-col items-start  mx-auto">
           <HeroLogo />
         </div>
-        <div className="flex  w-full  justify-between items-center z-30">
-          <div className=" flex flex-col justify-center items-end border-red-500 w-[25%] ">
-            <InfoIndicator />
-          </div>
+        <div className="flex flex-row-reverse sm:flex-row  w-full justify-between items-center  z-30">
+          <InfoIndicator />
 
-          <div className=" gap-10 w-[40%] ">
+          <div className=" flex  flex-col justify-center w-[60%] sm:w-[40%] ">
             <HeroContent />
 
             <StatisticSection />
--- src/components/Landing/hero-section/section/SearchBar.tsx
+++ src/components/Landing/hero-section/section/SearchBar.tsx
@@ -2,17 +2,17 @@
 import React, { useState, useRef, useEffect } from "react";
 import CommonSelect from "@/components/common/inputs/common/CommonSelect";
 import CommonButton from "@/components/common/buttons/common/CommonButton";
-import { Coins, MapPin, X } from "lucide-react";
+import { Coins, MapPin, Search, X } from "lucide-react";
 import CommonInput from "@/components/common/inputs/common/CommonInput";
 import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const searchRef = useRef<HTMLDivElement>(null);
-  const [transactionType, setTransactionType] = useState('')
-  const [minArea, setMinArea] = useState('')
-  const [maxArea, setMaxArea] = useState('')
-  const [location, setLocation] = useState('')
+  const [transactionType, setTransactionType] = useState("");
+  const [minArea, setMinArea] = useState("");
+  const [maxArea, setMaxArea] = useState("");
+  const [location, setLocation] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -31,7 +31,6 @@ const SearchBar = () => {
     };
   }, []);
 
-
   const handleClick = () => {
     const params = new URLSearchParams();
 
@@ -43,115 +42,137 @@ const SearchBar = () => {
     router.push(`/rent?${params.toString()}`);
   };
 
-
   return (
-    <div className="absolute bottom-12 left-0 right-0 mx-auto w-full max-w-5xl z-50 px-4">
+    <div className="absolute -bottom-2 lg:bottom-8 md:bottom-2 left-0 right-0 mx-auto w-full max-w-5xl z-50 px-4">
+      {/* Mobile Search Button */}
+      <div className="md:hidden flex justify-center mt-10 items-center gap-2 ">
+        <CommonButton
+          onclick={() => setIsOpen(true)}
+          title="جستجوی پیشرفته"
+          classname={`bg-primary text-secondary p-2 text-xs rounded-xl ${
+            isOpen ? "hidden" : "flex"
+          }`}
+          type="button"
+          icon={<Search size={12} />}
+        />
+      </div>
       <div
         ref={searchRef}
-        className={`flex justify-between items-center w-full bg-subBg backdrop-blur-sm rounded-[32px] p-4 transition-all duration-300 ${isOpen ? "h-auto opacity-100" : "h-0 overflow-visible md:h-auto"
-          } ${isOpen ? "opacity-100" : "opacity-0 md:opacity-100"}`}
+        className={`flex justify-between items-center w-full bg-subBg backdrop-blur-sm rounded-[32px] p-4 transition-all duration-300 ${
+          isOpen ? "h-auto opacity-100" : "h-0 overflow-visible md:h-auto"
+        } ${isOpen ? "opacity-100" : "opacity-0 md:opacity-100"}`}
       >
         <button
           onClick={() => setIsOpen(false)}
-          className={`md:hidden absolute top-2 left-2 text-gray-500 ${isOpen ? "block" : "hidden"
-            }`}
+          className={`md:hidden absolute top-2 left-2 text-gray-500 ${
+            isOpen ? "block" : "hidden"
+          }`}
         >
           <X size={20} />
         </button>
 
         <div
-          className={`flex-1 flex flex-col md:flex-row items-center justify-around gap-4 ${isOpen ? "block" : "hidden md:flex"
-            }`}
+          className={`flex-1 flex flex-col md:flex-row items-center justify-around gap-4 w-full ${
+            isOpen ? "block" : "hidden md:flex"
+          }`}
         >
-          <CommonSelect
-            placeholder="استان ، شهر ، هتل ...."
-            icon={<MapPin size={16} className="text-subText" />}
-            onValueChange={(value) => {
-              setLocation(value === "all" ? "" : value);
-            }}
-            selectItems={[
-              { label: "همه", value: "همه" },
-              { label: "آبادان", value: "آبادان" },
-              { label: "اراک", value: "اراک" },
-              { label: "ارومیه", value: "ارومیه" },
-              { label: "اردبیل", value: "اردبیل" },
-              { label: "اصفهان", value: "اصفهان" },
-              { label: "اهواز", value: "اهواز" },
-              { label: "ایلام", value: "ایلام" },
-              { label: "بجنورد", value: "بجنورد" },
-              { label: "بندرعباس", value: "بندرعباس" },
-              { label: "بوشهر", value: "بوشهر" },
-              { label: "بیرجند", value: "بیرجند" },
-              { label: "تبریز", value: "تبریز" },
-              { label: "تهران", value: "تهران" },
-              { label: "خرم‌آباد", value: "خرم‌آباد" },
-              { label: "رشت", value: "رشت" },
-              { label: "زاهدان", value: "زاهدان" },
-              { label: "زنجان", value: "زنجان" },
-              { label: "ساری", value: "ساری" },
-              { label: "سمنان", value: "سمنان" },
-              { label: "سنندج", value: "سنندج" },
-              { label: "شهرکرد", value: "شهرکرد" },
-              { label: "شیراز", value: "شیراز" },
-              { label: "قزوین", value: "قزوین" },
-              { label: "قم", value: "قم" },
-              { label: "کرج", value: "کرج" },
-              { label: "کرمان", value: "کرمان" },
-              { label: "کرمانشاه", value: "کرمانشاه" },
-              { label: "گرگان", value: "گرگان" },
-              { label: "مشهد", value: "مشهد" },
-              { label: "همدان", value: "همدان" },
-              { label: "یاسوج", value: "یاسوج" },
-              { label: "یزد", value: "یزد" },
-            ]}
+          <div className="w-full md:w-auto">
+            <CommonSelect
+              placeholder="استان ، شهر ، هتل ...."
+              icon={<MapPin size={16} className="text-subText" />}
+              onValueChange={(value) => {
+                setLocation(value === "all" ? "" : value);
+              }}
+              selectItems={[
+                { label: "همه", value: "همه" },
+                { label: "آبادان", value: "آبادان" },
+                { label: "اراک", value: "اراک" },
+                { label: "ارومیه", value: "ارومیه" },
+                { label: "اردبیل", value: "اردبیل" },
+                { label: "اصفهان", value: "اصفهان" },
+                { label: "اهواز", value: "اهواز" },
+                { label: "ایلام", value: "ایلام" },
+                { label: "بجنورد", value: "بجنورد" },
+                { label: "بندرعباس", value: "بندرعباس" },
+                { label: "بوشهر", value: "بوشهر" },
+                { label: "بیرجند", value: "بیرجند" },
+                { label: "تبریز", value: "تبریز" },
+                { label: "تهران", value: "تهران" },
+                { label: "خرم‌آباد", value: "خرم‌آباد" },
+                { label: "رشت", value: "رشت" },
+                { label: "زاهدان", value: "زاهدان" },
+                { label: "زنجان", value: "زنجان" },
+                { label: "ساری", value: "ساری" },
+                { label: "سمنان", value: "سمنان" },
+                { label: "سنندج", value: "سنندج" },
+                { label: "شهرکرد", value: "شهرکرد" },
+                { label: "شیراز", value: "شیراز" },
+                { label: "قزوین", value: "قزوین" },
+                { label: "قم", value: "قم" },
+                { label: "کرج", value: "کرج" },
+                { label: "کرمان", value: "کرمان" },
+                { label: "کرمانشاه", value: "کرمانشاه" },
+                { label: "گرگان", value: "گرگان" },
+                { label: "مشهد", value: "مشهد" },
+                { label: "همدان", value: "همدان" },
+                { label: "یاسوج", value: "یاسوج" },
+                { label: "یزد", value: "یزد" },
+              ]}
+              color="text-subText dark:group-hover:text-white"
+              label=" مقصد یا  هتل شما "
+              classname="px-4 py-5 border-subText w-full dark:group-hover:text-white dark:group-hover:border-white outline-none"
+            />
+          </div>
 
-            color="text-subText dark:group-hover:text-white"
-            label=" مقصد یا  هتل شما "
-            classname="px-4 py-5 border-subText w-full dark:group-hover:text-white dark:group-hover:border-white outline-none"
-          />
+          <div className="w-full md:w-auto">
+            <CommonSelect
+              onValueChange={(value) => {
+                setTransactionType(value === "all" ? "" : value);
+              }}
+              placeholder="رهن و اجاره"
+              icon={<Coins size={16} className="text-subText" />}
+              selectItems={[
+                { label: "رهن", value: "mortgage" },
+                { label: "اجاره", value: "rental" },
+                { label: "خرید", value: "direct_purchase" },
+                { label: "همه", value: "all" },
+              ]}
+              color="text-subText dark:group-hover:text-white"
+              label={"نوع ملک"}
+              classname="px-4 py-5 border-subText w-full dark:group-hover:text-white dark:group-hover:border-white outline-none"
+            />
+          </div>
 
-          <CommonSelect
-            onValueChange={(value => {
-              setTransactionType(value === "all" ? "" : value)
-            })}
-            placeholder="رهن و اجاره"
-            icon={<Coins size={16} className='text-subText' />}
-            selectItems={[
-              { label: "رهن", value: 'mortgage' },
-              { label: "اجاره", value: 'rental' },
-              { label: "خرید", value: 'direct_purchase' },
-              { label: "همه", value: "all" },
-            ]}
-            color='text-subText dark:group-hover:text-white'
-            label={"نوع ملک"}
-            classname='px-4 py-5 border-subText w-full dark:group-hover:text-white dark:group-hover:border-white outline-none'
-          />
+          <div className="w-full md:w-auto">
+            <CommonInput
+              onchange={(e) => setMinArea(e.target.value)}
+              label="حداقل متراژ"
+              classname="px-4 py-2 border-subText w-full outline-none"
+              color="text-subText placeholder:text-subText"
+              placeholder=" 0 متر "
+              type="number"
+            />
+          </div>
 
-          <CommonInput
-            onchange={(e) => setMinArea(e.target.value)}
-            label="حداقل متراژ"
-            classname="px-4 py-2 border-subText w-full outline-none"
-            color="text-subText placeholder:text-subText"
-            placeholder=" 0 متر "
-            type="number"
-          />
+          <div className="w-full md:w-auto">
+            <CommonInput
+              onchange={(e) => setMaxArea(e.target.value)}
+              label="حداکثر متراژ"
+              classname="px-4 py-2 border-subText w-full outline-none"
+              color="text-subText placeholder:text-subText"
+              placeholder=" 0 متر "
+              type="number"
+            />
+          </div>
 
-          <CommonInput
-            onchange={(e) => setMaxArea(e.target.value)}
-            label="حداگثر متراژ"
-            classname="px-4 py-2 border-subText w-full outline-none"
-            color="text-subText placeholder:text-subText"
-            placeholder=" 0 متر "
-            type="number"
-          />
-
-          <CommonButton
-            onclick={handleClick}
-            title="جستجو کن"
-            classname="px-8 py-7 text-sm font-bold text-primary-foreground"
-            type="button"
-          />
-
+          <div className="w-full md:w-auto">
+            <CommonButton
+              onclick={handleClick}
+              title="جستجو کن"
+              classname="px-8 py-7 text-sm font-bold text-primary-foreground w-full"
+              type="button"
+            />
+          </div>
         </div>
       </div>
     </div>
