47c8b166dacc1f152e8656ae385eab6350a8c9fc
Kian Janloo <ddrdslayer@gmail.com>
2025-05-21 17:16:00 +0330

    Debug

---
+++ src/components/dashboard/content/seller/dashboard/ComSellerDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComSellerDashboard = () => {
+  return (
+    <div className='px-4 bg-subBg rounded-xl py-4 flex flex-col gap-8'>
+      
+    </div>
+  )
+}
+
+export default ComSellerDashboard
